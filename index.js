9aab9b8d06888ac0ebcc73979248401551d56564
Khang <khang.nguyenduyvn@gmail.com>
2025-05-29 22:40:57 +0700

    Add bridge-1100

--- index.js
+++ index.js
@@ -67,4 +67,16 @@ function init(type) {
   } else {
     fetchData();
   }
+
+  bridge.send(target, {
+    event: "_init",
+    data: {
+      "vw-ratio": 1.3070696471734529,
+      color: {
+        root: { fg: "#a3b65c", bg: "#000000e6" },
+        inactive: { fg: "#4e5f46" },
+        night: { fg: "#6b762d", inactive: { fg: "#1c2119" } },
+      },
+    },
+  });
 }
